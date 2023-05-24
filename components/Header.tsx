42b45a654323892fea3278de816acc2fc39b48c4
N-Hidaya <nurul-h@hotmail.com>
2023-05-24 15:20:57 +0800

    Start from Zero

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,26 +5,11 @@ export default function Header({ photo }: { photo?: string | undefined }) {
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-2">
-        <Image
-          alt="header text"
-          src="/imageIcon.png"
-          className="sm:w-12 sm:h-12 w-7 h-7"
-          width={25}
-          height={25}
-        />
         <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          restorePhotos.io
+          YTtoMP3
         </h1>
       </Link>
-      {photo ? (
-        <Image
-          alt="Profile picture"
-          src={photo}
-          className="w-10 rounded-full"
-          width={32}
-          height={28}
-        />
-      ) : (
+      
         <a
           href="https://vercel.com/templates/next.js/ai-photo-restorer"
           target="_blank"
@@ -38,7 +23,7 @@ export default function Header({ photo }: { photo?: string | undefined }) {
             height={28}
           />
         </a>
-      )}
+      
     </header>
   );
 }
